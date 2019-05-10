c006f6495946b1c7b4f02bb15b2fa4874ac7d70f
fzh <fzh7500@gmail.com>
2019-05-10 18:45:43 +0800

    JDBC test

--- mybus/src/sql.java
+++ mybus/src/sql.java
@@ -1,13 +1,41 @@
 import java.sql.*;
+
 public class sql {
-        public static void main(String[] args){
+    static Connection conn = null;
+
+    public static void main(String[] args) {
+        LoadDriver();
+        getConn();
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("select ID from student");
+            while (rs.next()){
+                System.out.println(rs.getString("ID"));
+            }
+            rs.close();
+            conn.close();
+        } catch (Exception ex) {
+            System.out.println("error");
+        }
+    }
+
+    public static void LoadDriver() {
             try {
                 Class.forName("com.mysql.cj.jdbc.Driver");
-                String url="jdbc:mysql://localhost:3306/mybus?user=root&password=12345678";
-
-                Connection con = DriverManager.getConnection(url);
             } catch (Exception ex) {
             }
         }
 
+    static void getConn() {
+        try {
+            String url = "jdbc:mysql://localhost:3306/mybus?user=root&password=12345678" +
+                    "&serverTimezone=UTC";
+            conn = DriverManager.getConnection(url);
+            System.out.println(conn);
+        } catch (SQLException ex) {
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
     }
+}
