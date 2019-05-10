635efdf2368694a0011dc2eebd4e3ae145ab6701
fzh <fzh7500@gmail.com>
2019-05-10 13:11:59 +0800

    test

--- mybus/src/sql.java
+++ mybus/src/sql.java
@@ -1,2 +1,13 @@
+import java.sql.*;
 public class sql {
-}
+        public static void main(String[] args){
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                String url="jdbc:mysql://localhost:3306/mybus?user=root&password=12345678";
+
+                Connection con = DriverManager.getConnection(url);
+            } catch (Exception ex) {
+            }
+        }
+
+    }
