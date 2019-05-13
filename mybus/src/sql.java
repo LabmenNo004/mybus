8b26804ba5ef0440f143822c920494eb0e961b41
fzh <fzh7500@gmail.com>
2019-05-13 10:55:42 +0800

    增加身份认证接口

--- mybus/src/sql.java
+++ mybus/src/sql.java
@@ -1,10 +1,10 @@
 import java.sql.*;
 
 public class sql {
-    Connection conn = null;
-    ResultSet rs = null;
+    static Connection conn = null;
+    static ResultSet rs = null;
 
-    public ResultSet getResult(String sql){
+    public static ResultSet getResult(String sql){
         getConn();
         try {
             Statement stmt = conn.createStatement();
@@ -16,14 +16,14 @@ public class sql {
         close();
         return null;
     }
-    public void close(){
+    public static void close(){
         try {
             conn.close();
         } catch (Exception ex) {
             System.out.println("error");
         }
     }
-    public void getConn() {
+    public static void getConn() {
         try {
             String url = "jdbc:mysql://localhost:3306/mybus?user=root&password=12345678" +
                     "&serverTimezone=UTC";
@@ -33,6 +33,42 @@ public class sql {
             System.out.println("SQLException: " + ex.getMessage());
             System.out.println("SQLState: " + ex.getSQLState());
             System.out.println("VendorError: " + ex.getErrorCode());
+        }
+    }
+    public static int authentication(int type, String name, String password){
+        //type=1:user;type=2:driver;type=3:admin;
+        //return value is the ID; return -1 if authentication fails; return -2 if error occurs
+        String tableName=null;
+        String IDName=null;
+        switch (type){
+            case 1:{
+                tableName="user";
+                IDName="uid";
+                break;
+            }
+            case 2:{
+                tableName="driver";
+                IDName="driver_name";
+                break;
+            }
+            case 3:{
+                tableName="admin";
+                IDName="admin_id";
+            }
+        }
+        String sql="select "+IDName+" from "+tableName+" where "+tableName+"_name="+name+" and password="+password+";";
+        ResultSet rs=getResult(sql);
+        try{
+            if (rs.wasNull()) return -1;    //-------待测试--------
+            rs.next();
+            int temp=rs.getInt(IDName);
+            rs.close();
+            return temp;
+        } catch (SQLException ex) {
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+            return -2;
         }
     }
 }
