ea84a3b1f92cb85f5babd930cfc1e674d0eb9d1b
fzh <fzh7500@gmail.com>
2019-05-15 11:34:23 +0800

    test

--- mybus/src/main.java
+++ mybus/src/main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String [] args){
-        System.out.println("Hello! This is Smart Bus system.");
+        System.out.println("Hello! This is Smart Bus system terminal.");
         start();
     }
     public static void start(){
